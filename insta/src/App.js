fa2c86619c2403eb7fbb92627d1b039f70bddea6
kelvinrandu <randukelvin@gmail.com>
2021-06-29 14:56:15 +0300

    added post component

--- insta/src/App.js
+++ insta/src/App.js
@@ -1,13 +1,19 @@
-
-import './App.css';
+import "./App.css";
+import Post from "./Post";
 
 function App() {
   return (
-    <div className="App">
-        <p>
-          instagram clone
-        </p>
+    <div className="app">
+      <div className="app__header">
+        <img
+          className="app__header__image"
+          src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
+          alt="instagram"
+        ></img>
+      </div>
 
+      <Post />
+      <p>instagram clone</p>
     </div>
   );
 }
