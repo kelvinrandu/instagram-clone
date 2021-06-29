5ec29e0170cc8136f81d26cd1c77c5a6a2f0fead
kelvinrandu <randukelvin@gmail.com>
2021-06-30 01:11:58 +0300

    added signup button

--- insta/src/App.js
+++ insta/src/App.js
@@ -1,34 +1,108 @@
-import React, { useState,useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import Post from "./Post";
-import { db } from './firebase';
+import { db, auth } from "./firebase";
+import { makeStyles } from "@material-ui/core/styles";
+import Modal from "@material-ui/core/Modal";
+import { Button, Input } from "@material-ui/core";
+
+function getModalStyle() {
+  const top = 50;
+  const left = 50;
+
+  return {
+    top: `${top}%`,
+    left: `${left}%`,
+    transform: `translate(-${top}%, -${left}%)`,
+  };
+}
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    position: "absolute",
+    width: 400,
+    backgroundColor: theme.palette.background.paper,
+    border: "2px solid #000",
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
+}));
 function App() {
   const [posts, setPosts] = useState([]);
-  useEffect(()=>{
-    db.collection('posts').onSnapshot(snapshot => {
-      setPosts(snapshot.docs.map(doc => doc.data() ))
-    })
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const classes = useStyles();
+  // getModalStyle is not a pure function, we roll the style only on the first render
+  const [modalStyle] = React.useState(getModalStyle);
+  const [open, setOpen] = React.useState(false);
 
-  },[])
+  useEffect(() => {
+    db.collection("posts").onSnapshot((snapshot) => {
+      setPosts(
+        snapshot.docs.map((doc) => ({
+          id: doc.id,
+          post: doc.data(),
+        }))
+      );
+    });
+  }, []);
+  const handleSignup = (e) => {
+    e.preventDefault();
+    auth.createUserWithEmailAndPassword(email,password)
+    .catch((error) => alert(error.message))
+
+  };
   return (
     <div className="app">
+      <Modal open={open} onClose={() => setOpen(false)}>
+        <div style={modalStyle} className={classes.paper}>
+          <form className="app__signup">
+            <center>
+              <img
+                className="app__header__image"
+                src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
+                alt="instagram"
+              />
+            </center>
+            <Input
+              placeholder="username"
+              type="text"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+             {" "}
+            <Input
+              placeholder="email"
+              type="text"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <Input
+              placeholder="password"
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <Button type="submit" onClick={handleSignup}>sign up</Button>
+          </form>
+        </div>
+      </Modal>
       <div className="app__header">
         <img
           className="app__header__image"
           src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
           alt="instagram"
-        ></img>
+        />
       </div>
-      {posts.map((post)=>(
-              <Post
-              username={post.username}
-              imageUrl={post.imageUrl}
-              caption={post.caption}
-            />
-
+      <Button onClick={() => setOpen(true)}>sign up</Button>
+      {posts.map(({ id, post }) => (
+        <Post
+          key={id}
+          username={post.username}
+          imageUrl={post.imageUrl}
+          caption={post.caption}
+        />
       ))}
-
-
 
       <p>instagram clone</p>
     </div>
